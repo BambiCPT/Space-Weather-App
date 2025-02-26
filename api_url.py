cd1d9d82aee0773a8bba1c41685039657b6af96b
BambiCPT <deanobambi@gmail.com>
2025-02-27 01:02:04 +0200

    Added typehinting

--- api_url.py
+++ api_url.py
@@ -1,6 +1,6 @@
-xray_flares = "goes/primary/xray-flares-7-day.json"
-solar_probabilities = "solar_probabilities.json"
-planetary_k = "planetary_k_index_1m.json"
+XRAY_FLARES: str = "goes/primary/xray-flares-7-day.json"
+SOLAR_PROBABILITIES: str = "solar_probabilities.json"
+PLANETARY_K: str = "planetary_k_index_1m.json"
 
-def url (data_type):
+def url (data_type: str):
     return f"https://services.swpc.noaa.gov/json/{data_type}"
