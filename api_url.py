d018d3202b6188e0a3b3a82a64c23ea22fc72dcb
BambiCPT <deanobambi@gmail.com>
2025-02-27 00:44:02 +0200

    Added url function to return api endpoint based on input variable.

--- api_url.py
+++ api_url.py
@@ -1,7 +1,6 @@
 xray_flares = "goes/primary/xray-flares-7-day.json"
 solar_probabilities = "solar_probabilities.json"
 planetary_k = "planetary_k_index_1m.json"
-api_handles = [xray_flares, solar_probabilities, planetary_k]
 
 def url (data_type):
     return f"https://services.swpc.noaa.gov/json/{data_type}"
