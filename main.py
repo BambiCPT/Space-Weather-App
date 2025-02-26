be8cf1aeb5090707419be62d1d2a967939423f98
YohanStoffers <yohanstoffers18@gmail.com>
2025-02-26 23:51:29 +0100

    small fix

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 
 
 def main():
+    # to access the env varibles for the database
     load_dotenv()
     MY_DB_VAL = os.getenv('DB_PORT')
     print(MY_DB_VAL)
-
-
 
 
 main()
