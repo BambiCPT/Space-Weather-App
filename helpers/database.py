538afc57fa4cc646c11847c0eac993029dc13971
YohanStoffers <yohanstoffers18@gmail.com>
2025-02-27 22:40:53 +0100

    revert fix

--- helpers/database.py
+++ helpers/database.py
@@ -5,10 +5,7 @@ from config.db_config import db_config
 class MysqlConnector:
     def _connector(self):
         try:
-            db = mysql.connector.connect(**db_config)
-            return db
+            return mysql.connector.connect(**db_config)
         except Exception as e:
             print(f'DB connection error: {e}')
             return None
-        finally:
-            db.close()
