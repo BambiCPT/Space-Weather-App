3c8c69f2fc1c41b3e5e0b232f4926d095faf0d56
YohanStoffers <yohanstoffers18@gmail.com>
2025-02-27 22:57:02 +0100

    clean

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from helpers.database import MysqlConnector
-
-
 def main():
-    db = MysqlConnector()._connector()
-    cur = db.cursor()
-    cur.execute("SELECT * from test;")
-    print(cur.fetchall())
-    db.close()
+    print('test')
 
 
 main()
