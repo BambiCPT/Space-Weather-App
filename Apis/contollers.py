a14ba4dd60cbbea8d103cbe976bb393d738baf34
YohanStoffers <yohanstoffers18@gmail.com>
2025-03-01 14:48:30 +0100

    feat: added controllers file and made SwpcNoaa api class to call the different end points

---
+++ Apis/contollers.py
@@ -0,0 +1,26 @@
+import requests
+
+from helpers.api_url import Swpc, UrlEnums
+
+
+class SwpcNoaaApi():
+    def _get(self, url_param: UrlEnums) -> str | bool:
+        url = Swpc.get_url(url_param)
+        data = SwpcNoaaApi.__handel_call(url)
+        return data
+
+    @staticmethod
+    def __handel_call(url: str) -> requests.Response | bool:
+        try:
+            req = requests.get(url, timeout=10)
+            if req.status_code != 200 or req.headers['content-type'] != 'application/json':
+                print(f"Something went wrong while calling: {url}")
+                return False
+            if not req.json():
+                print("Error: Empty response")
+                return False
+            return req.json()
+
+        except Exception as e:
+            print(f"Call error: {e}")
+            return False
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from Apis.contollers import SwpcNoaaApi
+from helpers.api_url import UrlEnums
+
+
 def main():
-    print('test')
+    data_and_handled = SwpcNoaaApi()._get(UrlEnums.SOLAR_PROBABILITIES)
+    print(data_and_handled)
 
 
 main()
