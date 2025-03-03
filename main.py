df216f6c73966e62cdb45f8c790969bbde7709f4
BambiCPT <deanobambi@gmail.com>
2025-03-03 16:28:49 +0200

    Added some test values/funcs to main for the new functions

--- main.py
+++ main.py
@@ -14,8 +14,17 @@ def main():
         time="2025-03-01 17:58:00",
     )
 
+    # to test delete_by_id()
+    # print(db.delete_by_id('planetary_kp_indices', 2))
+
+    # to test get_all()
+    # print(db.get_all('planetary_kp_indices'))
+
+    # tests get_by_id() and also prints the before updated values
     print(db.get_by_id('planetary_kp_indices', 5))
+    # updates values using mock_data object
     db.update_by_id("planetary_kp_indices", 5, mock_data)
+    # shows the updated values vs the original values
     print(db.get_by_id('planetary_kp_indices', 5))
 
 
